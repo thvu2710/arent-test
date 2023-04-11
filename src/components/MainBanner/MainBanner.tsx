c926b0248e434498c0c35a8eb9b5acaba30e0e7c
steve <thvu2710@gmail.com>
2023-04-12 02:34:38 +0700

    fix: left banner width(40/60 left/right)

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -4,7 +4,7 @@ import './main-banner.scss'
 export default function MainBanner() {
   return (
     <div className='container mx-auto flex flex-row justify-center'>
-      <div className='left-banner' style={{ width: '75%' }}>
+      <div className='left-banner' style={{ width: '40%' }}>
         <CircleProgress percentage={75} date='05/21'></CircleProgress>
       </div>
       <div style={{ width: '60%' }}></div>
